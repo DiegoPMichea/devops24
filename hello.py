91f0e1d50b28a04f29c433e2d45474b84ae0b36e
Diego <pinomicheadiego@gmail.com>
2024-04-22 10:32:44 +0200

    Initial commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+# print("Hej")
+# namnet = input("Vad heter du? ")
+# age = input("Hur gammal är du? ")
+# print(f"Hej {namnet} du är {age} år")
+
+#import csv
+#import os
+
+#with open('employee_birthday.txt') as csv_file:
+#    csv_reader = csv.reader(csv_file, delimiter=',')
+#    line_count = 0
+#    for row in csv_reader:
+#        if line_count == 0:
+#            print(f'Column names are {", ".join(row)}')
+#            line_count += 1
+#        else:
+#            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+#            line_count += 1
+#    print(f'Processed {line_count} lines.')
+
+print("Hej")
